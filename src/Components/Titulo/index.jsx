a827aceed0cc9818ca37c87c7b8eac1eff30a4d5
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2022-01-30 14:27:00 -0300

    Add files via upload

--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import styled from 'styled-components'
+
+const Titulo = styled.h1 `
+`
 
 const Titulo = ({ children }) => {
   return <h1 className="titulo">{children}</h1>;
